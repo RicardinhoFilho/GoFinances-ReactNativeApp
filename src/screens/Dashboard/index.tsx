b5bd91491c8e115dba5e7b2660a84590dda1822a
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-06-07 07:16:07 -0300

    First COmmit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  Text } from 'react-native';
+import {
+    Container,Header
+} from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+        <Text>Ricardo</Text>
+            </Header>
+        </Container>
+    )
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,18 @@
+import { color } from 'react-native-reanimated';
+import {RFPercentage} from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+
+
+export const Container = styled.View`
+         
+        flex:1;
+        background-color:${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    height:${RFPercentage(42)}px;
+
+    background-color:${({theme})=>theme.colors.primary}
+`;
